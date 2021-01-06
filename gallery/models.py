d762b4f59eec67192e7eb330bb78802ae15a49c4
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-06 18:42:20 +0300

    add class for image

--- gallery/models.py
+++ gallery/models.py
@@ -31,5 +31,12 @@ class Category(models.Model):
 
 
 
-
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
 
