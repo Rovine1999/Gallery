c46cd5aa64879088d79404eaed5601b219f4f06b
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-08 17:40:37 +0300

    add save image test for save image

--- gallery/tests.py
+++ gallery/tests.py
@@ -17,5 +17,20 @@ class TestImage(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.image_test, Image))
 
-    # def test_save_method(self):
+    def test_save_method(self):
+        self.image_test.save_image()
+        image_test=Image.objects.all()
+        self.assertTrue(len(image_test) >0)
+
+    def test_save_method(self):
+        self.image_test.delete_image()
+        image_test=Image.objects.all()
+        self.assertTrue(len(image_test)== 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.new_image.id, 'photos/test.jpg')
+        changed_img = Image.objects.filter(image='photos/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
 
