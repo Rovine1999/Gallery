8562579ce97aed15cf06862b20b0a1d03d20121f
Rovine1999 <rovinewanjala99@gmail.com>
2021-01-08 14:54:12 +0300

    add image test for testing image methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Image,Location,Category
+
 
 # Create your tests here.
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='New York')
+        self.location.save_location()
+
+        self.category = Category(name='Home')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='photo', description='photo test', location=self.location,category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_method(self)
+    
